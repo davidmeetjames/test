054e7a56b919a5c5d18db252f7940d7adf35d451
AdaptedLogic Solutions Limited <adaptedlogic@gmail.com>
2021-01-23 09:09:28 +0100

    new Commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const favicon = require('express-favicon');
+var nodemailer = require('nodemailer');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
@@ -12,5 +13,30 @@ app.get('/ping', function (req, res) {
 });
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/sendmail', function (req, res) {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'davidmeetjames@gmail.com',
+      pass: 'Website24'
+    }
+  });
+
+  var mailOptions = {
+    from: 'WebDigit@gmail.com',
+    to: 'davidmeetjames@yahoo.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 });
 app.listen(port);
